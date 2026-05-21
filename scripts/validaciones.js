84b74f062b1192ce7d3add682d7169459a342968
elianaiaki <elianajaqueline06@gmail.com>
2026-05-21 16:56:36 -0300

    agregue archivo de validaciones

---
+++ scripts/validaciones.js
@@ -0,0 +1,23 @@
+function usuarioValido(usuario) {
+    return true;
+}
+ 
+function correoValido(correo){
+    return true;
+}
+
+function contraseniaValida(contrasenia) {
+    return true;
+}
+
+function textoAlfabeticoValido(texto){
+    return true;
+}
+
+function textoAlfanumericoValido(texto){
+    return true;
+}
+
+function numeroTelefonoValido(numero){
+    return true;
+}
